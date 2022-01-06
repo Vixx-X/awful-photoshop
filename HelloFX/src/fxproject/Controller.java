8cec98ffb54b39d0c2c5f963be61a2e3ca74ed4b
Gabriela Ustariz <gabrielaustariz@gmail.com>
2022-01-06 04:54:29 -0400

    added FileChooser

--- HelloFX/src/fxproject/Controller.java
+++ HelloFX/src/fxproject/Controller.java
@@ -1,7 +1,10 @@
 package fxproject;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
@@ -13,8 +16,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javax.imageio.ImageIO;
 
 public class Controller implements Initializable{
     
@@ -32,22 +37,28 @@ public class Controller implements Initializable{
 
 
     @FXML
-    void buttonClickMe(ActionEvent event) {
-        Image image = new Image(getClass().getResourceAsStream("../images/StarWars.png"));
-        imageView1.setImage(image);
-        imageView2.setImage(image);
+    void buttonClickMe(ActionEvent event) throws FileNotFoundException {
+        //Image image = new Image(getClass().getResourceAsStream("../images/StarWars.png"));
+        FileChooser fc = new FileChooser();
+        //fc.getExtensionFilters().add(new ExtensionFilter("*.pbm", "*.pgm", "*.bmp", "*.ppm", "*.pnm", "*.dib"));
+        List<File> f = fc.showOpenMultipleDialog(null);
+        Image[] images = new Image[2];
+        int i = 0;
+        for (File file: f){
+            images[i] = new Image(new FileInputStream(file.getAbsolutePath()));
+            System.out.println(images[i]);
+            i++;
+        }
+        //System.out.println(file.getAbsolutePath());
+        if (i>0) {
+            imageView1.setImage(images[0]);
+        }
+        if(i>1){
+            imageView2.setImage(images[1]);
+        }
+        
         System.out.println(imageView1);
         lab.setText("Holi");
-    }
-    
-    @FXML
-    void multipleFileChooser(ActionEvent event) {
-        FileChooser fc = new FileChooser();
-        fc.getExtensionFilters().add(new ExtensionFilter("PDF Files", "*.pdf"));
-        List<File> f = fc.showOpenMultipleDialog(null);
-        for (File file: f){
-            System.out.println(file.getAbsolutePath());
-        }
     }
 
     @Override
