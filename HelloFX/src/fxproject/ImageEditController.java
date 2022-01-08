1089c586e54251fc339f761dd6c843df1602177b
Gabriela Ustariz <gabrielaustariz@gmail.com>
2022-01-08 19:38:15 -0400

    added threshold methods and select by value or range

--- HelloFX/src/fxproject/ImageEditController.java
+++ HelloFX/src/fxproject/ImageEditController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 /**
@@ -26,6 +27,7 @@ public class ImageEditController implements Initializable {
 
     ObservableList<String> smoothedList = FXCollections.observableArrayList("Caja", "Cilíndrico", "Gauss");
     ObservableList<String> borderList = FXCollections.observableArrayList("Sobel", "Roberts", "Prewitt");
+    ObservableList<String> optionsThreshold = FXCollections.observableArrayList("Valor constante", "Rango");
     /**
      * Initializes the controller class.
      */
@@ -57,6 +59,21 @@ public class ImageEditController implements Initializable {
     private ImageView imageMain;
 
     @FXML
+    private ComboBox<String> threshold;
+
+    @FXML
+    private Label thresholdLabel;
+
+    @FXML
+    private TextField thresholdVal1;
+
+    @FXML
+    private TextField thresholdVal2;
+
+    @FXML
+    private Button thresholdButton;
+
+    @FXML
     void BlackWhiteFilter(ActionEvent event) {
         System.out.println("BlackWhiteFilter here");
         //imageMain.setImage([**insertar imagen de tipo Image**]);
@@ -83,15 +100,15 @@ public class ImageEditController implements Initializable {
     void enableFilterButton(ComboBox filterChoose, TextField col, TextField row, int number) {
         if (!(filterChoose.getValue() == null || col.getText().isEmpty()
                 || row.getText().isEmpty()) && validInputRowsColumns(col, row)) {
-            if(number == 0){
+            if (number == 0) {
                 buttonSmoothed.setDisable(false);
-            }else if(number == 1){
+            } else if (number == 1) {
                 buttonBorder.setDisable(false);
             }
-        }else{
-            if(number == 0){
+        } else {
+            if (number == 0) {
                 buttonSmoothed.setDisable(true);
-            }else if(number == 1){
+            } else if (number == 1) {
                 buttonBorder.setDisable(true);
             }
         }
@@ -99,28 +116,28 @@ public class ImageEditController implements Initializable {
 
     @FXML
     void selectFilters(ActionEvent event) {
-        if(event.getSource().equals(smoothedFilters)){
-            enableFilterButton(smoothedFilters,rowsSmooth,columnsSmooth, 0);
-        }else{
-            enableFilterButton(borderFilters,rowsBorder,columnsBorder, 1);
+        if (event.getSource().equals(smoothedFilters)) {
+            enableFilterButton(smoothedFilters, rowsSmooth, columnsSmooth, 0);
+        } else {
+            enableFilterButton(borderFilters, rowsBorder, columnsBorder, 1);
         }
     }
 
     @FXML
     void columnsFilters(ActionEvent event) {
-        if(event.getSource().equals(columnsSmooth)){
-            enableFilterButton(smoothedFilters,rowsSmooth,columnsSmooth, 0);
-        }else{
-            enableFilterButton(borderFilters,rowsBorder,columnsBorder, 1);
+        if (event.getSource().equals(columnsSmooth)) {
+            enableFilterButton(smoothedFilters, rowsSmooth, columnsSmooth, 0);
+        } else {
+            enableFilterButton(borderFilters, rowsBorder, columnsBorder, 1);
         }
     }
 
     @FXML
     void rowsFilters(ActionEvent event) {
-        if(event.getSource().equals(rowsSmooth)){
-            enableFilterButton(smoothedFilters,rowsSmooth,columnsSmooth, 0);
-        }else{
-            enableFilterButton(borderFilters,rowsBorder,columnsBorder, 1);
+        if (event.getSource().equals(rowsSmooth)) {
+            enableFilterButton(smoothedFilters, rowsSmooth, columnsSmooth, 0);
+        } else {
+            enableFilterButton(borderFilters, rowsBorder, columnsBorder, 1);
         }
     }
 
@@ -134,7 +151,7 @@ public class ImageEditController implements Initializable {
         System.out.println(smoothedFilters.getValue());
         //imageMain.setImage([**insertar imagen de tipo Image**]);
     }
-    
+
     @FXML
     void applyBorder(ActionEvent event) {
         int numberCol = Integer.parseInt(columnsBorder.getText());
@@ -144,6 +161,50 @@ public class ImageEditController implements Initializable {
         System.out.println(numberRow);
         System.out.println(borderFilters.getValue());
         //imageMain.setImage([**insertar imagen de tipo Image**]);
+    }
+
+    @FXML
+    void typeThreshold(ActionEvent event) {
+        thresholdLabel.setText(threshold.getValue());
+        if ("Rango".equals(threshold.getValue())) {
+            thresholdVal1.setVisible(true);
+            thresholdVal2.setVisible(true);
+        } else {
+            thresholdVal1.setVisible(true);
+            thresholdVal2.setVisible(false);
+        }
+    }
+    
+    @FXML
+    void enableThresholdButton(ActionEvent event) {
+        int min = Integer.parseInt(thresholdVal1.getText());
+        int max = 255;
+        if(!thresholdVal2.getText().isEmpty()){
+            max = Integer.parseInt(thresholdVal2.getText());
+        }
+        if ("Rango".equals(threshold.getValue()) && min >= 0 && min < 255
+                && max > min && max > 0 && max <= 255) {
+            thresholdButton.setDisable(false);
+        } else if("Valor constante".equals(threshold.getValue()) && min >= 0 && min <= 255 ){
+            thresholdButton.setDisable(false);
+        }else{
+            thresholdButton.setDisable(true);
+        }
+    }
+
+    @FXML
+    void applyThreshold(ActionEvent event) {
+        System.out.println("Umbralizacion aqui");
+        int min = Integer.parseInt(thresholdVal1.getText());
+         if ("Rango".equals(threshold.getValue())) {
+            int max = Integer.parseInt(thresholdVal2.getText());
+            System.out.println("rango");
+            System.out.println(min);
+            System.out.println(max);
+        } else {
+            System.out.println("valor");
+            System.out.println(min);
+        }
     }
 
     @FXML
@@ -159,6 +220,7 @@ public class ImageEditController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        threshold.setItems(optionsThreshold);
         smoothedFilters.setItems(smoothedList);
         borderFilters.setItems(borderList);
         imageMain.setImage(ProjectImages.getInstance().getImageChoose());
