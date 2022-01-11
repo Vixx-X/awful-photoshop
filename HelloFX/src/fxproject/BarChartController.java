33b08b3ea0591ec9da3c28c748353a3d4a40fc59
vixx_ <vittorio.adesso@gmail.com>
2022-01-10 21:20:25 -0400

    feat: info

--- HelloFX/src/fxproject/BarChartController.java
+++ HelloFX/src/fxproject/BarChartController.java
@@ -4,9 +4,7 @@
  */
 package fxproject;
 
-import fxproject.models.RawImage.Type;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,25 +13,25 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 
 public class BarChartController implements Initializable {
-    
+
     @FXML
     private BarChart histogram;
-    
+
     @FXML
     private Label labelHistogram;
-    
+
     private int[] dataHistogram;
     private final int typeHistogram;
-    
-    public BarChartController(){
+
+    public BarChartController() {
         this.typeHistogram = ProjectImages.getInstance().i;
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
-        switch(typeHistogram){
+
+        switch (typeHistogram) {
             case 3 -> {
                 dataHistogram = ProjectImages.getInstance().getChoose().getBlueHistogram();
                 labelHistogram.setText("Histograma del Canal Azul");
@@ -46,20 +44,21 @@ public class BarChartController implements Initializable {
                 dataHistogram = ProjectImages.getInstance().getChoose().getRedHistogram();
                 labelHistogram.setText("Histograma del Canal Rojo");
             }
-            case 0 -> dataHistogram = ProjectImages.getInstance().getChoose().getGrayHistogram();
-            default -> dataHistogram = ProjectImages.getInstance().getChoose().getGrayHistogram();
+            case 0 ->
+                dataHistogram = ProjectImages.getInstance().getChoose().getGrayHistogram();
+            default ->
+                dataHistogram = ProjectImages.getInstance().getChoose().getGrayHistogram();
         }
-        
-        
+
         //System.out.println("el histograma es" + Arrays.toString(dataHistogram));
         XYChart.Series dataSeries1 = new XYChart.Series();
         dataSeries1.getData().add(new XYChart.Data("0", dataHistogram[0]));
         //dataSeries1.setName("2014");
-        for(int i=1; i<dataHistogram.length -1; i++){
+        for (int i = 1; i < dataHistogram.length - 1; i++) {
             dataSeries1.getData().add(new XYChart.Data(String.valueOf(i), dataHistogram[i]));
         }
-        dataSeries1.getData().add(new XYChart.Data(String.valueOf(dataHistogram.length-1), dataHistogram[dataHistogram.length-1]));
-        
+        dataSeries1.getData().add(new XYChart.Data(String.valueOf(dataHistogram.length - 1), dataHistogram[dataHistogram.length - 1]));
+
         histogram.getData().add(dataSeries1);
     }
 }
